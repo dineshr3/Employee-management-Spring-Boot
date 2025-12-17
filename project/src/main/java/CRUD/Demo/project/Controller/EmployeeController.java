4e9696f13de04731ec8a8d88bad0ce22060551d2
Dinesh <rdinesh0313@example.com>
2025-12-17 22:37:15 +0530

    update url

--- project/src/main/java/CRUD/Demo/project/Controller/EmployeeController.java
+++ project/src/main/java/CRUD/Demo/project/Controller/EmployeeController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/user")
+@RequestMapping("/api/employee")
 public class EmployeeController {
 
     private EmployeeService employeeService;
